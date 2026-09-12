44c5867cd2eb5272f4ad34bfbb6045cc186cebe9
wilsonjosue <wturpoh@unsa.edu.pe>
2026-09-12 13:46:40 -0500

    Creamos el proyecto inicial con Jetpack Compose

--- Lab02/app/src/main/java/com/example/helloworldcompose/MainActivity.kt
+++ Lab02/app/src/main/java/com/example/helloworldcompose/MainActivity.kt
@@ -3,45 +3,18 @@ package com.example.helloworldcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.helloworldcompose.ui.theme.HelloWorldComposeTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            HelloWorldComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "WilsonT",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme {
+                Text("Registro de Lectura")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HelloWorldComposeTheme {
-        Greeting("Android")
     }
 }
